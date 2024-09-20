7d93cb57e01f8648521cb659df1b0a833f59351c
minseo585 <minseo585@naver.com>
2024-09-20 13:37:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-import max_profit as mp
-import prime_number as pn
+# 데이터 리스트
+data = [1, 3, 2, 4, 4, 5, 1, 3, 2, 1, 5, 5, 3, 4, 2, 1]
 
-if __name__ == '__main__':
-    pn.is_prime_number(1)
-    pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
+# 각 값의 빈도를 저장할 빈도수 딕셔너리
+frequency = {}
+
+# 데이터의 빈도 계산
+for num in data:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+
+# 최대 빈도 구하기
+max_freq = max(frequency.values())
+
+# 도수 히스토그램 세로로 출력
+print("세로 도수 히스토그램:")
+for i in range(max_freq, 0, -1):
+    for num in sorted(frequency.keys()):
+        if frequency[num] >= i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()  # 줄바꿈
+
+# 하단에 값 출력
+for num in sorted(frequency.keys()):
+    print(num, end=" ")
